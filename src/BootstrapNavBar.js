a37f9675114ddcb31f95aeef924821360931499a
Robert Bunch <robertbunch@Roberts-MacBook-Pro.local>
2019-02-19 10:25:11 -0500

    Add navbar active

--- src/BootstrapNavBar.js
+++ src/BootstrapNavBar.js
@@ -2,6 +2,19 @@ import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
 
 class BootstrapNavBar extends Component{
+	constructor(){
+		super();
+		this.state = {
+			activeLink: ""
+		}
+	}
+
+	setActiveLink(linkRoute){
+		this.setState({
+			activeLink: linkRoute
+		})
+	}
+
   render(){
     return(
 		<nav className="navbar navbar-default navbar-fixed-top">
@@ -11,10 +24,11 @@ class BootstrapNavBar extends Component{
 		    </div>
 		    <ul className="nav navbar-nav">
 		    	<li><Link to="/">Home</Link></li>
-		      	<li><Link to="/atlanta">Weather</Link></li>
-		      	<li><Link to="/mayormessage">Mayor Message</Link></li>
-		      	<li><Link to="/images">Images</Link></li>
-		      	<li><Link to="/about">About</Link></li>
+						<li><Link onClick={()=>{this.setActiveLink('/atlanta')}} to="/atlanta">
+						<span className={this.state.activeLink=='/atlanta' ? 'active' : ""}>Weather</span></Link></li>
+		      	<li><Link onClick={()=>{this.setActiveLink('/mayormessage')}} to="/mayormessage">Mayor Message</Link></li>
+		      	<li><Link onClick={()=>{this.setActiveLink('/images')}} to="/images">Images</Link></li>
+		      	<li><Link onClick={()=>{this.setActiveLink('/about')}} to="/about">About</Link></li>
 		    </ul>
 		  </div>
 		</nav>    
